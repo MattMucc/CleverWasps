6c7a86124e865470b85ba363775b7703857c4fa3
MattMucc <mattmucciacciaro118@gmail.com>
2023-10-28 14:57:19 -0400

    Added Script to UI
    Created the buttonFunctions script

--- Clever Wasps Project/Assets/Scripts/FloatingHealthBar.cs
+++ Clever Wasps Project/Assets/Scripts/FloatingHealthBar.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class FloatingHealthBar : MonoBehaviour
+public class floatingHealthBar : MonoBehaviour
 {
     [SerializeField] Slider slider;
     [SerializeField] Camera cam;
---
+++ Clever Wasps Project/Assets/Scripts/buttonFunctions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonFunctions : MonoBehaviour
+{
+    public void Resume()
+    {
+        gameManager.instance.stateUnpause();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameManager.instance.stateUnpause();
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
