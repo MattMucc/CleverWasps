c1564dc14db0b0fddd4b03c4ff5c40b5dc6e7b4f
mil1ky <messavij@gmail.com>
2023-10-30 14:49:27 -0400

    Enemy(AI)

--- Clever Wasps Project/Assets/Scripts/EnemyAI.cs
+++ Clever Wasps Project/Assets/Scripts/EnemyAI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour, IDamage
 {
@@ -24,12 +25,58 @@ public class EnemyAI : MonoBehaviour, IDamage
 
     void Start()
     {
-        
+        gameManager.instance.updateGameGoal(1);
+
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+
+        playerDir = gameManager.instance.player.transform.position - transform.position;
+       
+        if (!isShooting) 
+             StartCoroutine(shoot());
         
+            
+        
+            
+        if (agent.remainingDistance < agent.stoppingDistance)
+        {
+            faceTarget();
+        }
+        agent.SetDestination(gameManager.instance.player.transform.position);
+    }
+    IEnumerator shoot()
+    {
+        isShooting = true;
+
+        Instantiate(bullet, shootPos.position,transform.rotation);
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
+    }
+
+    public void takeDamage(int amount)
+    {
+        
+        HP -= amount;
+        StartCoroutine(flashRed());
+
+        if (HP <= 0)
+        {
+            gameManager.instance.updateGameGoal(-1);
+            Destroy(gameObject);
+        }
+    }
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = Color.white;
+    }
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(playerDir);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * playerFaceSpeed);// Time delta time is frame rate independent 
     }
 }
