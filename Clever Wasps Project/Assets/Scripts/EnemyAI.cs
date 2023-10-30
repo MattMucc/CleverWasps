d844b815d821ac220f3d41a4db0ad43b7da7fe94
mil1ky <messavij@gmail.com>
2023-10-30 14:37:55 -0400

    Enemy(AI) / Bullets

--- Clever Wasps Project/Assets/Scripts/EnemyAI.cs
+++ Clever Wasps Project/Assets/Scripts/EnemyAI.cs
@@ -2,9 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAI : MonoBehaviour
+public class EnemyAI : MonoBehaviour, IDamage
 {
-    // Start is called before the first frame update
+    [Header("---- Component ---")]
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Renderer model;
+    [SerializeField] Transform shootPos;
+   
+
+    [Header("---- Enemy Stats ---")]
+    [Range(1,10)][SerializeField] int HP;
+    [SerializeField] int playerFaceSpeed;
+
+    [Header("---- Blicky Stats ---")]
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootRate;
+    
+
+    Vector3 playerDir;
+    bool isShooting;
+
     void Start()
     {
         
