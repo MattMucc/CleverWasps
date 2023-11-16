11109e359ba38e09cfb4d43bc433578f1caffbe4
James Shaw <Jamesshaw1360@yahoo.com>
2023-11-16 14:14:36 -0500

    Final fixes to main
    ready for team pull

--- Clever Wasps Project/Assets/Scripts/gameManager.cs
+++ Clever Wasps Project/Assets/Scripts/gameManager.cs
@@ -12,18 +12,23 @@ public class gameManager : MonoBehaviour
     public static gameManager instance;
 
     [Header("----- Menu -----")]
-    [SerializeField] GameObject menuActive;
+    public GameObject menuActive;
     [SerializeField] GameObject menuPause;
     [SerializeField] GameObject menuWin;
-    [SerializeField] GameObject menuLose;
+    public  GameObject menuLose;
     [SerializeField] GameObject menuSettings;
-
-    [Header("----- Boss Settings")]
-    [SerializeField] EnemyAI boss;
 
     [Header("----- Settings Menu -----")]
     [SerializeField] Slider sensitivity;
     [SerializeField] TMP_Text sensitivityTextValue;
+
+    [Header("----- Gun UI -----")]
+    public Image reloadCircle;
+    public TMP_Text ammoText;
+
+    [Header("----- Boss Settings")]
+    [SerializeField] EnemyAI boss;
+    public Image bossHealthBar;
 
     [Header("----- Audio -----")]
     [SerializeField] soundController music;
@@ -55,10 +60,11 @@ public class gameManager : MonoBehaviour
     [SerializeField] Image healthBar;
     [SerializeField] TMP_Text enemyCount;
 
+    public bool isBossSpawned;
     public bool isPaused;
     float timescaleOrig;
     int enemiesRemaining;
-    
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -68,47 +74,57 @@ public class gameManager : MonoBehaviour
         playerScript = player.GetComponent<playerController>();
         playerSpawnPos = GameObject.FindWithTag("Respawn");
         cameraScript = Camera.main.GetComponent<CameraControls>();
-
+        reloadCircle = GameObject.Find("Circle Reload Concept").GetComponent<Image>();
         multiplier = 1;
         multiplierBar.fillAmount = 0;
         multiplierNumber.SetText("x" + multiplier.ToString());
         multiplierCoroutine = StartCoroutine(DecreaseMultiplier(multiplierResetTime));
 
-        enemiesRemaining = 0;
-        enemyCount.text = enemiesRemaining.ToString("0");
-
         sensitivity.value = cameraScript.Sensitivity;
         sensitivityTextValue.text = sensitivity.value.ToString();
 
+        if (GameObject.FindWithTag("Boss"))
+        {
+            isBossSpawned = true;
+            boss = GameObject.FindWithTag("Boss").GetComponent<EnemyAI>();
+        }
+        else
+            isBossSpawned = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         UpdateSettings();
-        if(Input.GetButtonDown("Cancel") && menuActive == null)
+        if (Input.GetButtonDown("Cancel") && menuActive == null)
         {
             statePause();
             menuActive = menuPause;
             menuActive.SetActive(isPaused);
         }
+
+        if (GameObject.FindWithTag("Boss") && !isBossSpawned)
+        {
+            isBossSpawned = true;
+            boss = GameObject.FindWithTag("Boss").GetComponent<EnemyAI>();
+        }
     }
 
     public void statePause()
     {
-        
+
         isPaused = !isPaused;
         Time.timeScale = 0;
         //grappleBarContainer.SetActive(false);
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Confined;
-              
-       
+
+
     }
 
     public void stateUnpause()
     {
-        
+
         isPaused = !isPaused;
         Time.timeScale = timescaleOrig;
         //grappleBarContainer.SetActive(true);
@@ -116,7 +132,7 @@ public class gameManager : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         menuActive.SetActive(false);
         menuActive = null;
-        
+
     }
 
     public void OpenSettings()
@@ -150,10 +166,12 @@ public class gameManager : MonoBehaviour
             UpdateMultiplier();
         }
 
-        
-        if (boss.HP <= 0)
+        if (boss != null)
         {
-            StartCoroutine(youWin());
+            if (boss.HP <= 0)
+            {
+                StartCoroutine(youWin());
+            }
         }
     }
 
@@ -236,15 +254,6 @@ public class gameManager : MonoBehaviour
         multiplierNumber.SetText("x" + multiplier.ToString());
     }
 
-
-    public playerController PlayerScript {get{return playerScript;}}
-    public GameObject PlayerSpawnPos {get {return playerSpawnPos;}}
-    public Image HealthBar {get{return healthBar;}}
-    public int Multiplier { get { return multiplier; } set { multiplier = value;} }
-    public float MultiplierAddValue { get { return multiplierAddedValue; } set { multiplierAddedValue = value; } }
-    public int MaxMultiplier { get { return maxMultiplier; } set { maxMultiplier = value; } }
-    public float MultiplierBar { get { return multiplierBar.fillAmount; } set { multiplierBar.fillAmount = value; } }
-
     public SoundAudioClip[] soundAudioClipArray;
 
     [System.Serializable]
@@ -254,4 +263,12 @@ public class gameManager : MonoBehaviour
         public AudioClip[] audioClips;
         public float audVolume;
     }
+
+    public playerController PlayerScript { get { return playerScript; } }
+    public GameObject PlayerSpawnPos { get { return playerSpawnPos; } }
+    public Image HealthBar { get { return healthBar; } }
+    public int Multiplier { get { return multiplier; } set { multiplier = value; } }
+    public float MultiplierAddValue { get { return multiplierAddedValue; } set { multiplierAddedValue = value; } }
+    public int MaxMultiplier { get { return maxMultiplier; } set { maxMultiplier = value; } }
+    public float MultiplierBar { get { return multiplierBar.fillAmount; } set { multiplierBar.fillAmount = value; } }
 }
